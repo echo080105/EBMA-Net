b8e1210b19893d14ddb2f378c653000892b5b714
舒心 <115856507+echo080105@users.noreply.github.com>
2025-04-18 23:53:32 +0800

    Update get_miou.py

--- get_miou.py
+++ get_miou.py
@@ -6,33 +6,15 @@ from tqdm import tqdm
 from unet import Unet
 from utils.utils_metrics import compute_mIoU, show_results
 
-'''
-进行指标评估需要注意以下几点：
-1、该文件生成的图为灰度图，因为值比较小，按照JPG形式的图看是没有显示效果的，所以看到近似全黑的图是正常的。
-2、该文件计算的是验证集的miou，当前该库将测试集当作验证集使用，不单独划分测试集
-3、仅有按照VOC格式数据训练的模型可以利用这个文件进行miou的计算。
-'''
+
 if __name__ == "__main__":
-    # ---------------------------------------------------------------------------#
-    #   miou_mode用于指定该文件运行时计算的内容
-    #   miou_mode为0代表整个miou计算流程，包括获得预测结果、计算miou。
-    #   miou_mode为1代表仅仅获得预测结果。
-    #   miou_mode为2代表仅仅计算miou。
-    # ---------------------------------------------------------------------------#
+    
     miou_mode = 0
-    # ------------------------------#
-    #   分类个数+1、如2+1
-    # ------------------------------#
+    
     num_classes = 4
-    # --------------------------------------------#
-    #   区分的种类，和json_to_dataset里面的一样
-    # --------------------------------------------#
+   
     name_classes = ["background", "Rust", "Slug", "Curl"]
-    # name_classes    = ["_background_","cat","dog"]
-    # -------------------------------------------------------#
-    #   指向VOC数据集所在的文件夹
-    #   默认指向根目录下的VOC数据集
-    # -------------------------------------------------------#
+    
     VOCdevkit_path = 'VOCdevkit'
 
     image_ids = open(os.path.join(VOCdevkit_path, "VOC2007/ImageSets/Segmentation/val.txt"), 'r').read().splitlines()
